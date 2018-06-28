8b2358db682925d9be892274d994ac51abc62ee0
Valerie Moore <periwinkle17@outlook.com>
2018-06-28 18:30:33 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,6 +28,6 @@ class CashRegister
       self.total = (1-(self.discount.to_f/100)) * self.total
       "After the discount, the total comes to $#{self.total.to_i}."
     end
-  end  
+  end
 
 end
